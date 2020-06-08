32302b7ffe697db3dd0460ab99098e43378179e8
ericwm76 <eric.meldrum@gmail.com>
2020-06-08 13:34:09 -0400

    Updates

---
+++ precache-manifest.ad045129ff75649f232e50e56558fc7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d575cf76d1af288a9ccadad5153896",
+    "url": "/colorgen/index.html"
+  },
+  {
+    "revision": "2799939b1e283fdc4701",
+    "url": "/colorgen/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "e52c1169abbc3562d4bc",
+    "url": "/colorgen/static/js/2.924b5c8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colorgen/static/js/2.924b5c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2799939b1e283fdc4701",
+    "url": "/colorgen/static/js/main.e341b6a4.chunk.js"
+  },
+  {
+    "revision": "7e8db6e1e5205ddf0bd8",
+    "url": "/colorgen/static/js/runtime-main.b601d4bf.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/colorgen/static/media/mintbean.cf18b629.png"
+  }
+]);
